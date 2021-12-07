830d33454900fcc9ed7f82bb5a4c51f33c268a49
mojotron <igormalj@gmail.com>
2021-12-07 20:10:39 +0100

    🚀 Initialize repo

---
+++ webpack.common.js
@@ -0,0 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/scripts/controller.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'main.[contenthash],js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+});
