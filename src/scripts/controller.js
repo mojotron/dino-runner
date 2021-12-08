2e63040f620bc7366041e80a928b2012587b302b
mojotron <igormalj@gmail.com>
2021-12-08 22:43:38 +0100

    Basic structer and test jump function

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,2 +1,18 @@
 import '../styles/reset.css';
 import '../styles/main.css';
+
+const dino = document.querySelector('.dino');
+
+window.addEventListener('keydown', () => {
+  const x = setInterval(() => {
+    if (dino.style.bottom) {
+      dino.style.bottom = `${parseInt(dino.style.bottom, 10) + 1}rem`;
+    } else {
+      dino.style.bottom = `1rem`;
+    }
+  }, 50);
+  setTimeout(() => {
+    clearInterval(x);
+    dino.style.bottom = '0rem';
+  }, 500);
+});
