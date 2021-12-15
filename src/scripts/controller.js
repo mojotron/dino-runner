388478d436fba7455809f71c7a70995b97c1e5a1
mojotron <igormalj@gmail.com>
2021-12-15 21:16:59 +0100

    Add score amd start game - fix some bugs

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -4,21 +4,38 @@ import '../styles/main.css';
 // selectors
 const sendBox = document.querySelector('.sand-box');
 const dino = document.querySelector('.dino');
+const score = document.querySelector('.score');
+const startGame = document.querySelector('.start-game');
 // global variables
 let isJumping = false;
 let gameOver = false;
+let gameRunning = false;
 let groundIndex = 0;
 let groundTimer;
 const intervals = [];
 
 const ground = document.querySelector('.ground-wrapper');
-
+function dinoRun() {
+  if (dino.classList.contains('dino-stationary')) {
+    dino.classList.remove('dino-stationary');
+    dino.classList.add('dino-run-0');
+  } else if (dino.classList.contains('dino-run-0')) {
+    dino.classList.remove('dino-run-0');
+    dino.classList.add('dino-run-1');
+  } else {
+    dino.classList.remove('dino-run-1');
+    dino.classList.add('dino-run-0');
+  }
+}
 const groundMovement = () => {
   groundTimer = setInterval(() => {
+    dinoRun();
     ground.style.left = `${-groundIndex}%`;
     groundIndex += 1;
     if (groundIndex === 100) groundIndex = 0;
-  }, 25);
+    const old = +score.textContent;
+    score.textContent = `${old + 1}`.padStart(5, '0');
+  }, 75);
 };
 
 // dino jump
@@ -26,7 +43,7 @@ const jump = () => {
   let count = 0;
   // going up
   const goingUp = setInterval(() => {
-    if (count === 15) {
+    if (count === 10) {
       clearInterval(goingUp);
       // going down
       const goingDown = setInterval(() => {
@@ -36,19 +53,21 @@ const jump = () => {
         }
         count -= 1;
         dino.style.bottom = `${count}rem`;
-      }, 20);
+        if (gameOver) clearInterval(goingDown);
+      }, 30);
     }
     count += 1;
     dino.style.bottom = `${count}rem`;
-  }, 20);
+    if (gameOver) clearInterval(goingUp);
+  }, 30);
 };
 
 const overlap = (a, b) => {
   if (
-    a.top > b.bottom ||
-    a.right < b.left ||
-    a.bottom < b.top ||
-    a.left > b.right
+    a.top - 15 > b.bottom ||
+    a.right - 15 < b.left ||
+    a.bottom - 15 < b.top ||
+    a.left - 15 > b.right
   ) {
     return false;
   }
@@ -56,7 +75,10 @@ const overlap = (a, b) => {
 };
 
 const createObstacle = () => {
+  if (gameOver) return;
+  const randomTime = Math.random() * 5000;
   const obstacle = document.createElement('div');
+
   obstacle.className = 'cactus';
   sendBox.append(obstacle);
 
@@ -75,25 +97,35 @@ const createObstacle = () => {
       sendBox.removeChild(obstacle);
     }
     if (overlap(dinoRect, obstacleRect)) {
+      intervals.forEach(int => clearInterval(int));
+      dino.className = 'dino dino-lose';
       isJumping = false;
       gameOver = true;
       position = null;
       clearInterval(groundTimer);
-      intervals.forEach(int => clearInterval(int));
       const temp = document.querySelectorAll('.cactus');
       temp.forEach(element => {
         const x = element.style.left;
         element.style.left = x;
       });
+      startGame.classList.remove('hidden');
+      gameRunning = false;
     }
-  }, 40);
-  if (!gameOver) setTimeout(createObstacle, Math.random() * 4000);
+  }, 15);
+  if (!gameOver) setTimeout(createObstacle, randomTime);
 };
-
-createObstacle();
-groundMovement();
+function runGame() {
+  document.querySelectorAll('.cactus').forEach(ele => ele.remove());
+  createObstacle();
+  groundMovement();
+  startGame.classList.add('hidden');
+}
 
 window.addEventListener('keydown', e => {
+  if (!gameRunning) {
+    gameRunning = true;
+    runGame();
+  }
   if (e.key === ' ') {
     if (!isJumping) {
       jump();
